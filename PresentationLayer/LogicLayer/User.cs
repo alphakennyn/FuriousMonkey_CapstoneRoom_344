231c0b1ecca348c6b00a3fc5a503a444de6391e9
Jacqueline <luo.jackie@hotmail.com>
2017-03-29 23:45:39 -0400

    Added attributes to User in LogicLayer
    Added the attributes corresponding to new requirements.

--- PresentationLayer/LogicLayer/User.cs
+++ PresentationLayer/LogicLayer/User.cs
@@ -6,12 +6,15 @@ using System.Threading.Tasks;
 
 namespace LogicLayer
 {
-  
-   public class User
+
+    public class User
     {
 
         public int userID { get; set; }
         public string name { get; set; }
+        public Boolean inCapstone { get; set; }
+        public int numOfReservations { get; set; }
+        public int hoursReservedInWeek { get; set; }
         
         public string username { get; set; }
         public string password { get; set; }
@@ -22,6 +25,9 @@ namespace LogicLayer
             username = "";
             password = "";
             name = "";
+            inCapstone = false;
+            numOfReservations = 0;
+            hoursReservedInWeek = 0;
         }
 
         public User (int idnumber, string un, string pw, string n)
