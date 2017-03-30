d0927c2e17cf70c1e096ff3d556a16d678679317
mosquitodawg <eric.payette24@gmail.com>
2017-03-30 11:15:47 -0400

    Put in equipment table and equipmentwaitsfor in database
    Also created directoryOfEquipment. Also i deleted 20 rooms and added 5 equipment.

--- PresentationLayer/StorageLayer/sql/user_sampledata.sql
+++
@@ -1,4 +0,0 @@
-INSERT INTO `user` VALUES ('1','h_ortiz','hortiz','Hannah Ortiz');
-INSERT INTO `user` VALUES ('2','i_koun','ikoun','Ideawin Koun');
-INSERT INTO `user` VALUES ('3','p_lim','plim','Philip Lim');
-INSERT INTO `user` VALUES ('4','admin','admin','Administrator');
