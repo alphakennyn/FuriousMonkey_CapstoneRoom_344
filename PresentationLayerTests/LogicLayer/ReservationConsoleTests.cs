de310274fc5f9f4d38d28abcde0c524a4c927489
Kenny Nguyen <alphakennyn@gmail.com>
2017-04-04 16:08:05 -0400

    Reservation Unit Testing complete

---
+++ PresentationLayerTests/LogicLayer/ReservationConsoleTests.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using LogicLayer;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogicLayer.SchedulerTests
+{
+    [TestClass()]
+    public class ReservationConsoleTests
+    {
+
+        [TestMethod()]
+        public void dailyConstraintCheckSucceedTest()
+        {
+            int firstHour = 1;
+            int lastHour = 3;
+
+            if (lastHour - firstHour > 3)
+            {
+                Assert.Fail();
+            }
+        }
+
+        [TestMethod()]
+        public void dailyConstraintCheckFailTest()
+        {
+            int firstHour = 1;
+            int lastHour = 7;
+
+            if (lastHour - firstHour <= 3)
+            {
+                Assert.Fail();
+            }
+        }
+    }
+}
