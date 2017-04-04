a92bd6502b08067a7d3d2d2bce5be6a29b4f22bd
Kenny Nguyen <alphakennyn@gmail.com>
2017-04-04 15:02:29 -0400

    Login Unit Testing

---
+++ PresentationLayerSchedulerTests/PresentationLayer/Controllers/AccountControllerTests.cs
@@ -0,0 +1,47 @@
+﻿using LogicLayer;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using PresentationLayer.Controllers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PresentationLayer.Controllers.SchedulerTests
+{
+    [TestClass()]
+    public class AccountControllerTests
+    {
+        [TestMethod()]
+        public void LoginTest()
+        {
+            string[] username = new String[] { "", "Jack", "Holly", "" };
+            string[] password = new String[] { "", "Smith", "Doe", "Doe" };
+
+            for (int i = 0; i < 4; i++)
+            {
+                if (username[i] == null || password[i] == null)
+                {
+                    Assert.Fail();
+                }
+            }
+
+        }
+
+        [TestMethod()]
+        public void LoginIsValidTest()
+        {
+            string username = "jack";
+            string password = "reacher";
+
+            foreach (User user in ReservationConsole.getInstance().getUserCatalog())
+            {
+                if (user.username != username && user.password != password)
+                {
+                    Assert.Fail();
+                }
+            }   
+
+        }
+    }
+}
